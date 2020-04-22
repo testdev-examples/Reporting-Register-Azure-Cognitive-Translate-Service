df4752a048b94b22d18ff81a5af0e4deddd101c4
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2020-04-23 02:27:50 +0300

    auto sync for version 2020.1

---
+++ CS/Reporting-Register-Azure-Cognitive-Translate-Service/translateexample/PredefinedReports/TestReport.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace translateexample.PredefinedReports
+{
+    public partial class TestReport
+    {
+        public TestReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
